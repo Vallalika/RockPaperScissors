856f66df19745812ba8d1ccd04232c4d370fd4c8
Valerie Bayard <valerie.bayard@gmail.com>
2022-04-16 19:55:32 +0100

    completed extension with basic css

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,16 +2,23 @@ from flask import render_template
 from app import app
 from models.player import Player
 from models.game import Game
-# from models.play_game import *
 
 @app.route('/')
 def home():
-    return 'Home'
+    return "Home"
+
+@app.route('/welcome')
+def welcome():
+    return render_template("welcome.html", title = "Rock-Paper-Scissors")
+
+@app.route('/play')
+def play():
+    return render_template("play.html", title = "Rock-Paper-Scissors")
 
 @app.route('/<choice1>/<choice2>')
-def game_result(choice1, choice2):
+def index(choice1, choice2):
     player1 = Player(choice1)
     player2 = Player(choice2)
     game = Game()
     results = game.play_game(player1,player2)
-    return render_template("index.html", title = "Game results", game_results = results, choice = choice1)
+    return render_template("index.html", title = "Rock-Paper-Scissors", game_results = results, choice = choice1)
