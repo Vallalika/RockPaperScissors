d593fffb52a05a4d4b113d6b6b41134cda9b938f
Valerie Bayard <valerie.bayard@gmail.com>
2022-04-16 14:52:32 +0100

    game rules coded

--- models/game.py
+++ models/game.py
@@ -2,4 +2,19 @@ class Game():
     def __init__(self, input_game_name):
         self.game_name = input_game_name
     
-    # def game(player1_choice, player2_choice):
+    def game(player1, player2):
+        
+        # ***** Game rules *****
+
+        winning_combo1 = (player1.choice == "paper") and (player2.choice == "rock")
+        winning_combo2 = (player1.choice == "rock") and (player2.choice == "scissors")
+        winning_combo3 = (player1.choice == "scissors") and (player2.choice == "paper")
+
+        if winning_combo1 or winning_combo2 or winning_combo3:
+            return True
+        
+        elif player1.choice == player2.choice:
+            return None
+        
+        else:
+            return False
